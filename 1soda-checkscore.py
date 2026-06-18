2593e9debba3914fafbeac39c87a79f00f6348b8
26820-gif <26820@bbw.ac.th>
2026-06-18 13:54:57 +0700

    1soda-checkscore.py

--- 1soda-checkscore.py
+++ 1soda-checkscore.py
@@ -1,8 +1,9 @@
+print("โปรเเกรมคำนวณคะเเนนรวม")
 score1 = int(input("ใส่คะเเนนวิชาที่1 0-100 "))
 score2 = int(input("ใส่คะเเนนวิชาที่2 0-100 "))
 score3 = int(input("ใส่คะเเนนวิชาที่3 0-100 "))
 allscore = (score1 + score2 + score3)/3
-print("คะเเนนที่ได้ คือ ",allscore)
+print("คะเเนนรวมที่ได้ คือ ",allscore,"คะเนน")
 if 60>allscore :print("ควรปรับปรุง")
 if 79<allscore :print("ดีเยี่ยม")
 if 80>allscore>59  :print("ผ่าน")
