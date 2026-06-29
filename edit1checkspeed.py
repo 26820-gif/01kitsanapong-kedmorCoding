3b82cfd11ba41881009cc23c8e673a7c1fe0fa4e
26820-gif <26820@bbw.ac.th>
2026-06-29 09:14:15 +0700

    โปรเเกรมตรวจสอบสถานะความเร็วรถ
    รับค่าข้อมูลความเร็วรถในหน่วย km/h เเละตรวจสอบความเร็วว่าอยู่ในสถานะไหนโดยมีเกณฑ์ ดังนี้
    ไม่เกิน 80  ปลอดภัย
    81 - 100  เตือน
    101 - 120  เสี่ยงถูกปรับ
    มากว่า 120  ผิดกฎหมาย(ปรับทันที)

---
+++ edit1checkspeed.py
@@ -0,0 +1,11 @@
+print("โปรเเกรมตรวจสอบสถานะความเร็วรถ")
+speed = float(input("่ค่าความเร็วรถ(km/h) "))
+print("ความเร็ว",speed,"อยู่ในสถานะ")
+if speed < 81 :
+    print("ปลอดภัย")
+elif speed < 101 :
+    print("เตือน")
+elif speed < 121 :
+    print("เสี่ยงถูกปรับ")
+else :
+    print("ผิดกฎหมาย(ปรับทันที)")
