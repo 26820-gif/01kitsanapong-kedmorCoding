060616b619cc27e616f7fe80d15871050d3d26eb
26820-gif <26820@bbw.ac.th>
2026-07-06 09:05:04 +0700

    โปรเเกรมสูตรคูณ เวอร์ชั่น1
    กรอกเเม่สูตรคูณที่ค้องการทราบเเล้วโปรเเกรมจะเเสดงผลเเม่สูตรคูณให้

---
+++ 1soda-Multiplication table.py
@@ -0,0 +1,6 @@
+print("โปรเเกรมสูตรคูณ")
+number = int(input("สูตรคูณที่ค้องการทราบ คือ เเม่ "))
+print("\nสูตรคูณ เเม่",number,"คือ")
+for i in range (1,13):
+    print(number,"*",i,"=",number * i)
+print("\nจัดทำโดย นายกฤษณพงศ์ เกิดเหมาะ")
